49fd098e02983ac701cd61cad52800b24749219c
Ayush Biswas <ayushbiswascob@gmail.com>
2025-01-26 15:47:05 +0530

    Multiplication Program

---
+++ Programs/Multiplication.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a,b;
+    printf("Enter the first number: ");
+    scanf("%d",&a);
+    printf("Enter the second number: ");
+    scanf("%d",&b);
+    printf("The multiplication of %d and %d is %d",a,b,a*b);
+    getch();
+}
