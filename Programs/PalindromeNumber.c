7a8e9e21c673dee5c7cfe2234263f0ccc0730e24
Ayush Biswas <ayushbiswascob@gmail.com>
2025-02-07 12:06:50 +0530

    conflict resolve

--- Programs/PalindromeNumber.c
+++ Programs/PalindromeNumber.c
@@ -1,1 +1,17 @@
 #include <stdio.h>
+void main() {
+    int n,r,sum=0,temp;
+    printf("Enter a number: ");
+    scanf("%d",&n);
+    temp=n;
+    while(n>0) {
+        r=n%10;
+        sum=(sum*10)+r;
+        n=n/10;
+    }
+    if(sum==temp)
+        printf("Is a Palindrome Number");
+    else
+        printf("Is not a Palindrome Number");
+    return 0;
+}
